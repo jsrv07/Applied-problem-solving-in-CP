e1cdfe95a1ed1754ff182c844b480747d9c094f2
Shreshtha <shreshthagupta2111@gmail.com>
2025-06-25 19:30:05 +0530

    week5 fin

--- Shreshtha_24b1033/week5/valid_anagram.cpp
+++ Shreshtha_24b1033/week5/valid_anagram.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
+        unordered_map<char,int>mapST,mapTS;
+        for(char ch:s){
+            mapST[ch]++;
+        }
+        for(char ch:t){
+            mapTS[ch]++;
+        }
+        return mapST==mapTS;
     }
 };
